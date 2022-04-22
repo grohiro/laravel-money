8e68d96dc84ba01683bfac91bcf7bd0d11f31713
grohiro <khirokawa@growk.jp>
2022-04-22 14:29:59 +0900

    ifndef

--- src/functions.php
+++ src/functions.php
@@ -4,32 +4,40 @@ use Money\Currency;
 use Money\Money;
 use Grohiro\LaravelMoney\MoneyHelper;
 
-/**
- * @param int $amount
- * @param string|Currency $currency
- * @return Money 
- */
-function money($amount, $currency) : Money
-{
-    if (is_string($currency)) {
-        $cc = currency($currency);
-    } else {
-        $cc = $currency;
+if (!function_exists('money')) {
+    /**
+    * @param int $amount
+    * @param string|Currency $currency
+    * @return Money
+    */
+    function money($amount, $currency) : Money
+    {
+        if (is_string($currency)) {
+            $cc = currency($currency);
+        } else {
+            $cc = $currency;
+        }
+        return new Money($amount, $cc);
     }
-    return new Money($amount, $cc);
 }
 
-function currency($code)
-{
-    return new Currency($code);
+if (!function_exists('currency')) {
+    function currency($code)
+    {
+        return new Currency($code);
+    }
 }
 
-function format_money(Money $money)
-{
-    return MoneyHelper::decimalFormatter()->format($money);
+if (!function_exists('format_money')) {
+    function format_money(Money $money)
+    {
+        return MoneyHelper::decimalFormatter()->format($money);
+    }
 }
 
-function json_money(Money $money)
-{
-    return MoneyHelper::json($money);
+if (!function_exists('json_money')) {
+    function json_money(Money $money)
+    {
+        return MoneyHelper::json($money);
+    }
 }
